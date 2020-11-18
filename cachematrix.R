eb4b1eebdbe314e6fd23044eb2a2c33ed6e28f58
jcjim7916 <jose@MacBook-Pro-de-Jose.local>
2020-11-18 11:00:57 -0600

    Mis dos programas de la asignacion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Crea una matriz especial que puede almacenar en caché su inversa
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(M = matrix()) {
 
+  a <- NULL
+  set <- function(y){
+    M <<- y
+    a <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) a <<- inverse
+  getInverse <- function() a
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+## Esta función devuelve la inversa de la mariz, Usa la función anterior. 
+## La inversa se calcula con la función solve()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  a <- x$getInverse()
+  if(!is.null(a)){
+    message("getting cached data")
+    return(a)
+  }
+  matriz <- x$get()
+  a <- solve(matriz,...)
+  x$setInverse(a)
+  a
 }
